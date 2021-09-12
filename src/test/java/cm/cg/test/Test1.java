28cd92c21d2b290c44b2f29d6465c1a1940345f8
rupa <rupasreeachari1999@gmail.com>
2021-09-12 16:59:27 +0530

    first commit

---
+++ src/test/java/cm/cg/test/Test1.java
@@ -0,0 +1,13 @@
+package cm.cg.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+public class Test1 {
+	
+	@Test
+	public void test() {
+		assertEquals("Hello", "Hello");
+	}
+
+}
